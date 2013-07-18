681acf0078551b139392cdca6538f1fb6326c38b
Kilian Van den Eynde <kilianvde@gmail.com>
2013-07-18 18:34:38 +0200

    Trigger build commit

--- kilian/ModXFramework/ModX.java
+++ kilian/ModXFramework/ModX.java
@@ -3,7 +3,6 @@ package kilian.ModXFramework;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.network.NetworkMod;
 
-
 @Mod(modid="VAModXFramework",name="VirtualArts ModX Framework",version="v0.1.0")
 @NetworkMod(clientSideRequired=true,serverSideRequired=false)
 public class ModX {
